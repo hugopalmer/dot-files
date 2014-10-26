2da710c0784ea60eb8f6b837cbc7da0d4f184af2
Remi Paulmier <remi.paulmier@gmail.com>
2014-10-26 18:20:41 +0100

    added chef/knife config

---
+++ dot-chef/knife.rb
@@ -0,0 +1,18 @@
+log_level            :info
+log_location         STDOUT
+node_name            'rpaulmier'
+cache_type           'BasicFile'
+cache_options( :path => "/Users/remi/.chef/checksums" )
+client_key           '/Users/remi/.chef/rpaulmier.pem'
+
+#cXXXookbook_path       [ "/Users/remi/stash/chef/chef/cookbooks" ]
+# oh-my-zsh knife plugin for auto-completion needs the array to not have spaces
+cookbook_path        ['/Users/remi/stash/chef/chef/cookbooks']
+cookbook_copyright   "BlaBlaCar"
+cookbook_email       "remi.paulmier@blablacar.com"
+cookbook_license     "apachev2"
+
+chef_server_url      "https://vbbcchef1.bourse-prod.prive.comuto.com"
+validation_key       "/Users/remi/.chef/validation.pem"
+
+knife[:ssh_user] = "root"
